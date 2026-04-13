4e62321b567d6d08925cb5fad43bc332bacde908
igruk9 <igruk4459@gmail.com>
2026-04-13 22:23:33 +0300

    PRJ-43 base project architecture added.

---
+++ App/Database/DatabaseManager.cs
@@ -0,0 +1,22 @@
+using Microsoft.Data.Sqlite;
+
+namespace MyRoguelikeBot.Database;
+
+public class DatabaseManager
+{
+    private string _connectionString = "Data Source=game.db";
+
+    public void InitDatabase()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Players (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                TelegramId INTEGER UNIQUE,
+                Nickname TEXT
+            )";
+        command.ExecuteNonQuery();
+    }
+}
