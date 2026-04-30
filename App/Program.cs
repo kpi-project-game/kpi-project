9f007a6739c7e653e23cb7be7a462b0110fe9ae8
igruk9 <igruk4459@gmail.com>
2026-04-30 03:47:24 +0300

    PRJ-59: Done.

--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,13 @@
-﻿using KPI_PROJECT.Models.FirstLevelEnemies;
+﻿using KPI_PROJECT.Database;
+using KPI_PROJECT.Models.FirstLevelEnemies;
 using KPI_PROJECT.Telegram.Handlers;
 
 class Program
 {
     static async Task Main(string[] args)
     {
+        var dbManager = new DatabaseManager();
+        dbManager.InitDatabase();
         BotManager botManager = new BotManager();
         await botManager.StartAsync();
 
