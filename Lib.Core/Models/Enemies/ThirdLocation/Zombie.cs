c990763d95db0f9916f88fbd4c5592f29170bee0
wqep <deni20072008@gmail.com>
2026-05-18 22:17:56 +0300

    PRJ-49 Done.

--- Lib.Core/Models/Enemies/ThirdLocation/Zombie.cs
+++ Lib.Core/Models/Enemies/ThirdLocation/Zombie.cs
@@ -6,17 +6,17 @@ namespace Lib.Core.Models.Enemies.ThirdLocation;
 
 public class Zombie : EnemyBase, IBattleUnit
 {
-    // public Zombie()
-    // {
-    //     Hp = 70;
-    //     MaxHp = 70;
-    //     HandDmg = 12;
-    //     Name = "Zombie";
-    //     PhisDefense = 8;
-    //     CurrentSkills.Add(new Frenzy(2));
-    //     CurrentSkills.Add(new Frighten());
-    //     CurrentSkills.Add(new Bite(15));
-    // }
+    public Zombie()
+    {
+        Hp = 70;
+        MaxHp = 70;
+        HandDmg = 12;
+        Name = "Zombie";
+        PhisDefense = 8;
+        CurrentSkills.Add(new Frenzy(2));
+        CurrentSkills.Add(new Frighten());
+        CurrentSkills.Add(new Bite(15));
+    }
     
     public override void CastSkill(ISkill chosenSkill, IBattleUnit target)
     {
